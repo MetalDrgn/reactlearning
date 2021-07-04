8cc08a6d56075b26f64e42d410bae7fd1f299ded
Ryan <draco124@hotmail.com>
2021-07-04 10:30:02 -0700

    a

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
 import "./App.css";
-
+import { useState, useRef, useEffect } from "react";
+function test() {}
 function App() {
+  const [name, setName] = useState("");
+  // ------------
+  // --Without useRef
+  const inputRef = useRef();
+  function Focus() {
+    inputRef.current.focus();
+  }
+  // ------------
+  // ------------
+  // --With useRef
+  // const renderCount = useRef(1);
+
+  // useEffect(() => {
+  //   renderCount.current = renderCount.current + 1;
+  // });
+  // ----------------
   return (
     <div className="App">
       <header className="App-header">
+        <input
+          type="number"
+          value={name}
+          ref={inputRef}
+          onChange={(e) => setName(e.target.value)}
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>input is {name}</p>
+        <button onClick={Focus}>Focus</button>
+        {/* <div>I rendered {inputRef.current} times</div> */}
+        {/* <div>I render {renderCount.current} times</div> */}
       </header>
     </div>
   );
