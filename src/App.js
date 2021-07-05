40cb2a6c01000157b6c2ea03aea0212320b27948
Ryan <draco124@hotmail.com>
2021-07-05 11:45:28 -0700

    1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { useState, useRef, useEffect } from "react";
-function Test() {
-  const [name, setName] = useState("");
+function Test({ name, setName, id }) {
+  // const [name, setName] = useState("");
   // ------------
   // --Without useRef
   const inputRef = useRef();
@@ -12,14 +12,25 @@ function Test() {
     <>
       <input
         type="number"
-        value={name}
+        // value={name[id]}
         ref={inputRef}
-        onChange={(e) => setName(e.target.value)}
+        onChange={(e) =>
+          setName((asd) => {
+            // console.log(asd);
+            let zxc = [...asd];
+            // console.log(typeof asd);
+            // console.log(Array.isArray(asd));
+            zxc[id] = e.target.value;
+            // zxc = [e.target.value];
+            return zxc;
+          })
+        }
+        // onChange={(e) => setName(e.target.value)}
       />
       <p>
         Edit <code>src/App.js</code> and save to reload.
       </p>
-      <p>input is {name}</p>
+      <p>input is {name[id]}</p>
       <button onClick={Focus}>Focus</button>
       {/* <div>I rendered {inputRef.current} times</div> */}
       {/* <div>I render {renderCount.current} times</div> */}
@@ -28,6 +39,7 @@ function Test() {
   );
 }
 function App() {
+  const [name, setName] = useState([]);
   // ------------
   // --With useRef
   // const renderCount = useRef(1);
@@ -39,8 +51,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Test />
-        <Test />
+        <Test key={0} id={0} name={name} setName={setName} />
+        <Test key={1} id={1} name={name} setName={setName} />
       </header>
     </div>
   );
